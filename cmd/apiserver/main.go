54cc3e1a3daef2024c55d7628a997bb918c2184a
yykhomenko <yykhomenko@gmail.com>
2020-06-12 23:43:47 +0300

    added config file and parse it

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
+
+	"github.com/BurntSushi/toml"
 
 	"github.com/yykhomenko/http-rest-api/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "config/apiserver.toml", "path to config file")
+}
+
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/yykhomenko/http-rest-api
 
 go 1.14
 
-require github.com/BurntSushi/toml v0.3.1 // indirect
+require github.com/BurntSushi/toml v0.3.1
