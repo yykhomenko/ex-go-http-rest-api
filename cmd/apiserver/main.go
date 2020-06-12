b4cd981adc140a7862b5be0c7a0473103b0cf62f
yykhomenko <yykhomenko@gmail.com>
2020-06-12 23:17:25 +0300

    added apiserver.go

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/yykhomenko/http-rest-api/internal/app/apiserver"
+)
+
+func main() {
+	s := apiserver.New()
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
