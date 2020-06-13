79678d67f1d9a81ca38d55b9190491ac8babc562
yykhomenko <yykhomenko@gmail.com>
2020-06-13 10:37:53 +0300

    added logger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yykhomenko/http-rest-api
 
 go 1.14
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.6.0
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,41 @@
 package apiserver
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 // APIServer ...
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 // New ...
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start ...
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
