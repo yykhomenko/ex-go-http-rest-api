28720c5568ca4e3afe55c2a8b4fe46ebfd139c5d
yykhomenko <yykhomenko@gmail.com>
2020-06-13 14:14:23 +0300

    added User insert code

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -9,7 +9,15 @@ type UserRepository struct {
 
 // Create ...
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
-	return nil, nil
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING ID",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+
+	return u, nil
 }
 
 // FindByEmail ...
