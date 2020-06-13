71edf3d3cd27b48ef4c1467c5cc1971c97c2837b
yykhomenko <yykhomenko@gmail.com>
2020-06-13 14:56:54 +0300

    added TestStore test

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -1,1 +1,22 @@
 package store_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/yykhomenko/http-rest-api/internal/app/model"
+	"github.com/yykhomenko/http-rest-api/internal/app/store"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
