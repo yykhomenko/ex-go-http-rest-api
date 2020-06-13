259ddfa4f57b34b8205a9b81663d8a4663113152
yykhomenko <yykhomenko@gmail.com>
2020-06-13 17:19:20 +0300

    added test BeforeCreate User

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -1,1 +1,15 @@
 package model_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/yykhomenko/http-rest-api/internal/app/model"
+)
+
+func TestUser_BeforeCreate(t *testing.T) {
+	u := model.TestUser(t)
+	assert.NoError(t, u.BeforeCreate())
+	assert.NotEmpty(t, u.EncryptedPassword)
+}
