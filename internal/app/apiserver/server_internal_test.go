d7bc9193f3d1ef0e1ef5651d49c0e535d600c6c0
yykhomenko <yykhomenko@gmail.com>
2020-06-13 21:35:22 +0300

    ref server_internal_test.go

---
+++ internal/app/apiserver/server_internal_test.go
@@ -0,0 +1,19 @@
+package apiserver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/yykhomenko/http-rest-api/internal/app/store/teststore"
+)
+
+func TestServer_HandleUsersCreate(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+	assert.Equal(t, rec.Code, http.StatusOK)
+}
