8adcd7bbf95c5cdde66e8910382c5e8cfcbd61e7
yykhomenko <yykhomenko@gmail.com>
2020-06-13 20:15:52 +0300

    ref userrepository_test.go

--- internal/app/store/sqlstore/testing.go
+++ internal/app/store/sqlstore/testing.go
@@ -2,6 +2,7 @@ package sqlstore
 
 import (
 	"database/sql"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -21,7 +22,7 @@ func TestDB(t *testing.T, databaseURL string) (*sql.DB, func(...string)) {
 
 	return db, func(tables ...string) {
 		if len(tables) > 0 {
-			db.Exec("TRUNCATE %s CASCADE", strings.Join(tables, ", "))
+			db.Exec(fmt.Sprintf("TRUNCATE %s CASCADE", strings.Join(tables, ", ")))
 		}
 
 		db.Close()
