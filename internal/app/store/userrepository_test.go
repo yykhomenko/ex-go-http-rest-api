ffb7aa7934ede1d70c9fa586466f027c7d6f72ec
yykhomenko <yykhomenko@gmail.com>
2020-06-13 15:13:39 +0300

    added TestUserRepository_FindByEmail

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -20,3 +20,20 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
