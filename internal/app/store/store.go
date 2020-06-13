3caf0db8a4b818f6fd8668c0b334f4e9ad620b36
yykhomenko <yykhomenko@gmail.com>
2020-06-13 13:54:50 +0300

    added User() func

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -8,8 +8,9 @@ import (
 
 // Store ...
 type Store struct {
-	config *Config
-	db     *sql.DB
+	config         *Config
+	db             *sql.DB
+	userRepository *UserRepository
 }
 
 // New ...
@@ -38,4 +39,17 @@ func (s *Store) Open() error {
 // Close ...
 func (s *Store) Close() {
 	s.db.Close()
+}
+
+// Store ...
+func (s *Store) User() *UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		s,
+	}
+
+	return s.userRepository
 }
