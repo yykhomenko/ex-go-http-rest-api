65cc6921e7c89915676fd66df2390cc637e90a4f
yykhomenko <yykhomenko@gmail.com>
2020-06-13 13:09:53 +0300

    added user Create stub

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -1,6 +1,13 @@
 package store
 
+import "github.com/yykhomenko/http-rest-api/internal/app/model"
+
 // UserRepository
 type UserRepository struct {
 	store *Store
+}
+
+// Create ...
+func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	return nil, nil
 }
